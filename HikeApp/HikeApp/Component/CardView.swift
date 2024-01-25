2c98de9db56c13e078e8a62765976987e702d5f6
Sheep1sik <bohg6799@naver.com>
2024-01-25 20:06:19 +0900

    Feat: CardView CustomBackgroundView 추가

--- HikeApp/HikeApp/Component/CardView.swift
+++ HikeApp/HikeApp/Component/CardView.swift
@@ -9,24 +9,30 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
+        // MARK: - CARD
+        
         ZStack {
-            Circle()
-                .fill(
-                    LinearGradient(
-                        colors: [
-                            Color("ColorIndigoMedium"),
-                            Color("ColorSalmonLigth")
-                        ],
-                        startPoint: .topLeading,
-                        endPoint: .bottomTrailing
+            CustomBackgroundView()
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            colors: [
+                                Color("ColorIndigoMedium"),
+                                Color("ColorSalmonLigth")
+                            ],
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                        )
                     )
-                )
-                .frame(width: 256, height: 256)
-            
-            Image("image-1")
-                .resizable()
-            .scaledToFit()
-        }
+                    .frame(width: 256, height: 256)
+                
+                Image("image-1")
+                    .resizable()
+                .scaledToFit()
+            }
+        } //: CARD
+        .frame(width: 320, height: 570)
     }
 }
 
