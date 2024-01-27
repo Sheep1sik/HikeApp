91c12640d6cdff9e04fcb0202e69c611372a91fd
Sheep1sik <bohg6799@naver.com>
2024-01-27 17:10:17 +0900

    SwiftUI List and Labeled Content 구현

--- HikeApp/HikeApp/Screen/SettingView.swift
+++ HikeApp/HikeApp/Screen/SettingView.swift
@@ -47,6 +47,25 @@ struct SettingView: View {
             // MARK: - SECTION: ICONS
             
             // MARK: - SECTION: ABOUT
+            Section(
+                header: Text("ABOUT THE APP"),
+                footer: HStack {
+                    Spacer()
+                    Text("Copyright © All right reserved")
+                    Spacer()
+                }.padding(.vertical, 8)
+            ) {
+                // 1. Basic Labeled Content
+                // LabeledContent("Application", value: "Hike")
+                // 2. Advanced Labeled Content
+                CustomListRowView(rowLabel: "Application", rowIcon: "apps.iphone", rowContent: "Hike", rowTintColor: .blue)
+                CustomListRowView(rowLabel: "Compatibility", rowIcon: "info.circle", rowContent: "iOS,iPadOS", rowTintColor: .red)
+                CustomListRowView(rowLabel: "Technology", rowIcon: "swift", rowContent: "Swift", rowTintColor: .orange)
+                CustomListRowView(rowLabel: "Version", rowIcon: "gear", rowContent: "1.0", rowTintColor: .purple)
+                CustomListRowView(rowLabel: "Developer", rowIcon: "ellipsis.curlybraces", rowContent: "_GGDol", rowTintColor: .mint)
+                CustomListRowView(rowLabel: "Designer", rowIcon: "paintpalette", rowContent: "_GGDol", rowTintColor: .pink)
+                CustomListRowView(rowLabel: "GitHub", rowIcon: "globe", rowTintColor: .indigo, rowLinkLabel: "_GGDol", rowLinkDestination: "https://github.com/Sheep1sik")
+            } //: SECTION
         } //: LIST
     }
 }
